cb1b89bada43c1a0175a11794fcbbc06450576c4
sadia-jannat <37320382+sadia-jannat@users.noreply.github.com>
2022-02-26 09:06:20 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,43 +1,29 @@
-from django.contrib import admin
+"""first_project URL Configuration
 
-from django.conf.urls import url
-from . import views
-from django.urls import path
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import  admin
+from django.urls import include, path
+
+from django.conf import  settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-
-    path('',views.home,name='food-home'),
-    path('about/',views.about,name='food-about'),
-    path('bmi/',views.bmi,name='bmi'),
-    path('chart/',views.chart,name='chart'),
-    path('other/',views.other,name='other'),
-    path('add/',views.add,name='add'),
-    path('res/',views.res,name='res'),
-
-    path('cal', views.cal, name='calculation'),
-
-    path('math/',views.math,name='math'),
-
-    path('teatment/', views.teatment, name='teatment'),
-
-    path('brain/', views.brain, name='brain'),
-
-    path('health/', views.health, name='health'),
-
-    path('food/',views.food,name='food'),
-
-    path('women/', views.women, name='women'),
-
-    #pactis 2
-
-    path('welcome/', views.welcome, name='welcome'),
-
-
-
-
-
-
-
-
-
+    path('admin/', admin.site.urls),
+    path('food/',include('food.urls')),
+    path('blog/',include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
